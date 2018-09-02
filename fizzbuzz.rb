ee959f52f5ebacef6ea13afbf71b6d6e1c106c21
Max Petersen <mglxp5@gmail.com>
2018-09-02 19:05:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
-def divisable(x,y)
-  if (x % )
+def divAble(x,y)
+  if (x % y == 0)
+    return TRUE
+  else
+    return FALSE
+  end
+end
 
 def fizzbuzz(n)
   a = 3
   b = 5
+  if(divAble(n,a) && divAble(n,b))
+    return
